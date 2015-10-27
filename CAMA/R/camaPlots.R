9308cc401c167049e614e2f2838705d9d982306c
Rafael <rafael.2706@gmail.com>
2015-10-27 23:42:06 +0100

    grafo adicionado

--- CAMA/R/camaPlots.R
+++ CAMA/R/camaPlots.R
@@ -1,4 +1,4 @@
-camaPlots <- function(camaList, verbose=TRUE, outputDir=NULL) {
+camaPlots <- function(camaList, verbose=TRUE, labels=NULL, outputDir=NULL) {
   # WARNING----------------------------------------\
   # Please, note that these are just quick scripts!!
   # They are not ready for everyday consumption!!!!!
@@ -9,6 +9,9 @@ camaPlots <- function(camaList, verbose=TRUE, outputDir=NULL) {
   # Just a quick wrap up. Very inneficient. Need to change before final version
   estDAG <- camaList[[1]]
   inData <- camaList[[2]]
+  
+  # Variable names
+  labels=labels
   
   # Now, the real thing...
   if(verbose) {  
@@ -24,7 +27,7 @@ camaPlots <- function(camaList, verbose=TRUE, outputDir=NULL) {
   ###### ADD YOUR CODE HERE!
   qgraph(estDAG,type="factorial",legend=FALSE,vsize=7.75,
          label.scale=F,label.cex=1,edge.color = "black",
-         transparency = F,bg=F,diag=F,mar=c(2,2,2,2),vTrans=235,arrows=T)
+         transparency = F, labels=labels, bg=F,diag=F,mar=c(2,2,2,2),vTrans=235,arrows=T)
   
   
   # If the user requires to write the plots to files...
